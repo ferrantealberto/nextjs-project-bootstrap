99fa5ae1c2d405955674ba26f41e877a5048aad8
ferrantealberto <143766026+ferrantealberto@users.noreply.github.com>
2025-06-04 13:34:57 +0200

    Add .next/static/chunks/src_app_pattern_page_tsx_a993527f._.js 1749036897148

---
+++ .next/static/chunks/src_app_pattern_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_e67873ef._.js",
+  "static/chunks/node_modules_ec28e85e._.js"
+],
+    source: "dynamic"
+});
