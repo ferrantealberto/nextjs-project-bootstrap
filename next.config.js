b497b3822c6eb3ac99ee095db98049c33576451f
ferrantealberto <143766026+ferrantealberto@users.noreply.github.com>
2025-06-04 13:34:34 +0200

    Add next.config.js 1749036874463

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  allowedDevOrigins: ['gj3dg4-8000.csb.app']
+}
+
+module.exports = nextConfig
